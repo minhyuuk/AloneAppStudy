3e838d5c409e60ce7e3132d0fb66fabfa89b35e9
ZzukMa <67806878+ZzukMa@users.noreply.github.com>
2020-12-16 23:43:53 +0900

    TextChangedListener 추가

--- app/src/main/java/checkmyskill/OpenUrl.kt
+++ app/src/main/java/checkmyskill/OpenUrl.kt
@@ -2,11 +2,30 @@ package checkmyskill
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
 import com.example.myapplication.R
+import kotlinx.android.synthetic.main.activity_openurl.*
 
 class OpenUrl : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_openurl)
+
+        EditText.addTextChangedListener(object : TextWatcher {
+            override fun afterTextChanged(p0: Editable?) {
+
+                
+
+            }
+            override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+
+            }
+            override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+
+            }
+        })
     }
+
+
 }
