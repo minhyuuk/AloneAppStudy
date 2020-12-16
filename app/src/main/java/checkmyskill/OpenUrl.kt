9eb8cab22a76cc008083bb0ed35f3fa7b85926aa
ZzukMa <67806878+ZzukMa@users.noreply.github.com>
2020-12-16 21:04:06 +0900

    응용 문제

---
+++ app/src/main/java/checkmyskill/OpenUrl.kt
@@ -0,0 +1,12 @@
+package checkmyskill
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.myapplication.R
+
+class OpenUrl : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_openurl)
+    }
+}
