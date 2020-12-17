d7c4cc6a19e933ff3955e1d4957f29ddf570a51d
ZzukMa <67806878+ZzukMa@users.noreply.github.com>
2020-12-17 20:40:33 +0900

    Openning Internet Page

--- app/src/main/java/checkmyskill/OpenUrl.kt
+++ app/src/main/java/checkmyskill/OpenUrl.kt
@@ -1,9 +1,12 @@
 package checkmyskill
 
+import android.content.Intent
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.text.Editable
 import android.text.TextWatcher
+import android.util.Log
 import com.example.myapplication.R
 import kotlinx.android.synthetic.main.activity_openurl.*
 
@@ -12,19 +15,37 @@ class OpenUrl : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_openurl)
 
-        EditText.addTextChangedListener(object : TextWatcher {
-            override fun afterTextChanged(p0: Editable?) {
 
+        // 버튼을 누르면 실행되는 코드
+        Open.setOnClickListener{
+            val address = EditText.text.toString() // 문자를 string 형으로 받아온다
+            val intent = Intent(Intent.ACTION_VIEW, Uri.parse(address)) // intent에 address의 값을 받아온다
+            startActivity(intent) // activity를 실행한다
+        }
 
+        // EditText의 글자를 인식하는 메서드 오버라이딩
+        EditText.addTextChangedListener(object : TextWatcher{
 
+            // Log를 찍으며 의미를 파악할 수 있음
+            override fun afterTextChanged(s: Editable?) {
+                if(s.toString() == "abc") {
+                    Log.d("edit", "text is abc!")
+                }
+                Log.d("edit", "afterTextChanged : $s")
             }
-            override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
 
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+                Log.d("edit", "beforeTextChanged : $s")
             }
-            override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
 
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                Log.d("edit", "onTextChanged : $s")
             }
         })
+
+
+
+
     }
 
 
