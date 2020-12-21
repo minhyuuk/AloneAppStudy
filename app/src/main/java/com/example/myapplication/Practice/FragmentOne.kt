40b82b2e9e9bf0aa2fd136b9f9e73d1d9085c473
ZzukMa <67806878+ZzukMa@users.noreply.github.com>
2020-12-21 10:36:01 +0900

    Dipping logs

--- app/src/main/java/com/example/myapplication/Practice/FragmentOne.kt
+++ app/src/main/java/com/example/myapplication/Practice/FragmentOne.kt
@@ -7,17 +7,19 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import com.example.myapplication.R
+import kotlinx.android.synthetic.main.activity_fragment.*
 
 
 class FragmentOne : Fragment() {
 
     override fun onAttach(context: Context) {
-        Log.d("Lifecycle","onCreate")
+        Log.d("Lifecycle","F onCreate")
         super.onAttach(context)
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
-        Log.d("Lifecycle","onCreate")
+        Log.d("Lifecycle","F onCreate")
         super.onCreate(savedInstanceState)
     }
 
@@ -26,47 +28,53 @@ class FragmentOne : Fragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        Log.d("Lifecycle","onCreate")
-        return super.onCreateView(inflater, container, savedInstanceState)
+        Log.d("Lifecycle","F onCreateView")
+        // fragment가 인터페이스를 처음으로 그릴 때 호출된다
+
+        // inflater  -> 뷰를 그려주는 친구
+        // container -> 부모 뷰
+
+        return inflater.inflate(R.layout.fragment_one,container,false)
+
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        Log.d("Lifecycle","onCreate")
+        Log.d("Lifecycle","F onCreate")
         super.onViewCreated(view, savedInstanceState)
     }
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
-        Log.d("Lifecycle","onCreate")
+        Log.d("Lifecycle","F onCreate")
         super.onActivityCreated(savedInstanceState)
     }
 
     override fun onStart() {
-        Log.d("Lifecycle","onStart")
+        Log.d("Lifecycle","F onStart")
         super.onStart()
     }
 
     override fun onResume() {
-        Log.d("Lifecycle","onResume")
+        Log.d("Lifecycle","F onResume")
         super.onResume()
     }
 
     override fun onPause() {
-        Log.d("Lifecycle","onPause")
+        Log.d("Lifecycle","F onPause")
         super.onPause()
     }
 
     override fun onStop() {
-        Log.d("Lifecycle","onStop")
+        Log.d("Lifecycle","F onStop")
         super.onStop()
     }
 
     override fun onDestroyView() {
-        Log.d("Lifecycle","onDestroyView")
+        Log.d("Lifecycle","F onDestroyView")
         super.onDestroyView()
     }
 
     override fun onDetach() {
-        Log.d("Lifecycle","onDetach")
+        Log.d("Lifecycle","F onDetach")
         super.onDetach()
     }
 }
