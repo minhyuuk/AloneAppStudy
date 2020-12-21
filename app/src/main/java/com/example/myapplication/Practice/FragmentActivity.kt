fcd467c5b9b58fe27baa26d002a1339b627ca0b6
ZzukMa <67806878+ZzukMa@users.noreply.github.com>
2020-12-21 21:00:58 +0900

    이해가 잘 안되는 Fragment

--- app/src/main/java/com/example/myapplication/Practice/FragmentActivity.kt
+++ app/src/main/java/com/example/myapplication/Practice/FragmentActivity.kt
@@ -3,13 +3,37 @@ package com.example.myapplication.Practice
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
 import com.example.myapplication.R
+import kotlinx.android.synthetic.main.activity_fragment.*
 
 class FragmentActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_fragment)
         Log.d("Lifecycle","onCreate")
+
+        button.setOnClickListener{
+            // Fragment를 동적으로 작동하는 방법
+            val fragmentOne : FragmentOne = FragmentOne()
+            // supportFragmentManager의 부모가 AppCompatActivity이므로 사용이 바로 가능함
+            val fragmentManager : FragmentManager = supportFragmentManager
+            // Transaction
+            // 작업의 단위라 생각하면 된다.  ->  ( 시작과 끝이 있다 )
+
+            // 시작
+            val fragmentTransaction = fragmentManager.beginTransaction()
+            // 할 일
+            fragmentTransaction.replace(R.id.container, fragmentOne)
+            // 끝
+            fragmentTransaction.commit()
+
+            // 끝을 내는 방법
+            // commit    -> 시간 될 때 해라  (좀 더 안정적이다)
+            // commitnow -> 지금 당장 하여라
+        }
+
     }
     override fun onStart() {
         super.onStart()
