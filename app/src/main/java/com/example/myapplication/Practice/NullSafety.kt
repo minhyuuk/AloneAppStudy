5d729750c7e3c1057b265a4a0405652a5519348e
ZzukMa <67806878+ZzukMa@users.noreply.github.com>
2020-12-28 20:57:47 +0900

    lateinit

--- app/src/main/java/com/example/myapplication/Practice/NullSafety.kt
+++ app/src/main/java/com/example/myapplication/Practice/NullSafety.kt
@@ -6,6 +6,7 @@ import android.util.Log
 import com.example.myapplication.R
 
 class NullSafety : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_null_safety)
@@ -17,8 +18,9 @@ class NullSafety : AppCompatActivity() {
           val number3 = number1?.plus(number)
           Log.d("number","number3 : " + number3)
 //        삼항연산자 -> 엘비스 연산자(?:)
+//        Null Safety 를 위한 도구
           val number4 = number1 ?: 10
-          Log.d("number","number4 : " + number3)
+          Log.d("number","number4 : " + number4)
     }
     fun plus (a : Int, b : Int?) : Int {
         if(b != null) return a + b
