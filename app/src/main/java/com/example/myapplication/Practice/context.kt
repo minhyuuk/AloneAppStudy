83d3af28d021e415a9b546a529693aec7d455bce
ZzukMa <67806878+ZzukMa@users.noreply.github.com>
2020-12-31 07:28:34 +0900

    context 수정

--- app/src/main/java/com/example/myapplication/Practice/context.kt
+++ app/src/main/java/com/example/myapplication/Practice/context.kt
@@ -1,5 +1,6 @@
 package com.example.myapplication.Practice
 
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.myapplication.R
@@ -8,5 +9,9 @@ class context : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_context)
+
+        val context : Context = this
+        val applicationContext = getApplication()
+
     }
 }
