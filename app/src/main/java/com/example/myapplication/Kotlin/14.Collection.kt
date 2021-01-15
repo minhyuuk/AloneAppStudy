4b88554cdcc9a67088f1eb8d3cf41408e932e583
ZzukMa <67806878+ZzukMa@users.noreply.github.com>
2021-01-15 20:36:54 +0900

    Update 14.Collection.kt

--- app/src/main/java/com/example/myapplication/Kotlin/14.Collection.kt
+++ app/src/main/java/com/example/myapplication/Kotlin/14.Collection.kt
@@ -2,6 +2,7 @@ package com.example.myapplication.Kotlin
 
 fun main(args:Array<String>){
 
+    // Immutable Collections (변경 불가)
     // List -> 중복을 허용한다
     val numberList = listOf<Int>(1,2,3,3)
     println(numberList)
@@ -10,9 +11,26 @@ fun main(args:Array<String>){
     // -> 집합과 같음. 중복을 허용하지 않는다
     // -> 순서가 없다.
     val numberSet = setOf<Int>(1,2,3,3,3)
-    println(numberSet)
+
+    numberSet.forEach{
+        println(it)
+    }
 
     // Map -> key value 방식 관리
     val numberMap = mapOf<String,Int>("one" to 1, "two" to 2)
+
+    // Mutable Collections (변경 가능)
+    val mNumberList = mutableListOf<Int>(1,2,3)
+    mNumberList.add(3,4)
+    println()
+    println(mNumberList)
+
+    val mNumberSet = mutableSetOf<Int>(1,2,3,4,4,4)
+    mNumberSet.add(10)
+    println(mNumberSet)
+
+
+
+
 
 }
