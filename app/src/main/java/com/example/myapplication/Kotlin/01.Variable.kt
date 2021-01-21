5cf9c8f9f1fed3c2c928d2d60fc63ff97cdbd2ac
ZzukMa <67806878+ZzukMa@users.noreply.github.com>
2021-01-21 22:08:52 +0900

    Update 01.Variable.kt

--- app/src/main/java/com/example/myapplication/Kotlin/01.Variable.kt
+++ app/src/main/java/com/example/myapplication/Kotlin/01.Variable.kt
@@ -1,13 +1,36 @@
 package com.example.myapplication.Kotlin
 
 // 01 . Variable -> 변수
-//
+// var / val 변수명(상자) = 값 (넣고 싶은 것)
+// var -> variable를 줄인 말. 다양한 이라는 뜻으로 값을 변형시킬 수 있다.
+// val -> value를 줄인 말. 값 이라는 뜻으로 값을 변형시킬 수 없다.
 
 
-
+var num1 = 10
+var hello1 = "hello1"
+var point1 = 3.4
+val num2 = 20
+val hello2 = "hello2"
+val point2 = 6.4
 
 fun main(args:Array<String>){
 
+    println(num1)
+    println(hello1)
+    println(point1)
+    println(num2)
+    println(hello2)
+    println(point2)
     
+
+//  var은 값을 변형시킬 수 있다. 바꾸어도 오류가 나지 않는다.
+    num1 = 100
+    hello1 = "goodbye1"
+    point1 = 100.1
+
+//  val은 값을 변형시킬 수 없다. 바꾸면 오류가 난다.
+//  num2 = 40
+//  hello2 = "goodbye2"
+//  point2 = 999.4
 
 }
