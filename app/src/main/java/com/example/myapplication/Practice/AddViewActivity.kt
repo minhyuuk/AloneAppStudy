cff0134e8ad23ca1f198ab1bcf3e276278b3c92d
ZzukMa <67806878+ZzukMa@users.noreply.github.com>
2021-01-23 18:44:37 +0900

    View 추가

---
+++ app/src/main/java/com/example/myapplication/Practice/AddViewActivity.kt
@@ -0,0 +1,42 @@
+package com.example.myapplication.Practice
+
+import android.content.Context
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.widget.LinearLayout
+import android.widget.TextView
+import com.example.myapplication.R
+
+class AddViewActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_add_view)
+
+        val carList = ArrayList<CarForList>()
+        for (i in 0 until 10) {
+            carList.add(CarForList("" + i + "번째 자동차", "" + i + "순위 엔진"))
+        }
+
+        val container = findViewById<LinearLayout>(R.id.addview_container)
+        val inflater = LayoutInflater.from(this@AddViewActivity)
+
+        for (i in 0 until carList.size) {
+            val itemView = inflater.inflate(R.layout.item_view, null)
+            val carNameView = itemView.findViewById<TextView>(R.id.car_name)
+            val carEngineView = itemView.findViewById<TextView>(R.id.car_engine)
+
+            carNameView.setText(carList.get(i).name)
+            carEngineView.setText(carList.get(i).engine)
+
+            container.addView(itemView)
+        }
+
+    }
+
+
+}
+
+class CarForList(val name: String, val engine: String) {
+
+}
