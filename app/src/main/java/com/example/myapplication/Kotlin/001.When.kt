e4db64fd374033ab08565edc247ca0f51ec41a6e
ZzukMa <67806878+ZzukMa@users.noreply.github.com>
2021-02-02 21:50:27 +0900

    when에 대해

---
+++ app/src/main/java/com/example/myapplication/Kotlin/001.When.kt
@@ -0,0 +1,22 @@
+package com.example.myapplication.Kotlin
+
+import android.util.Log
+
+
+//    var now = 10
+//
+//
+//    when (now) {
+//
+//    8  -> {
+//        Log.d("when", "현재 시간은 8시 입니다.")
+//    }
+//
+//    9 -> {
+//        Log.d("when","현재 시간은 9시 입니다.")
+//    }
+//    else -> {
+//        Log.d("when","현재 시간은 9시가 아닙니다.")
+//    }
+//
+//    }
