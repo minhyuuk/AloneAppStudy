127321148f0bca8c8800d13bcb5a9990184a8546
ZzukMa <67806878+ZzukMa@users.noreply.github.com>
2021-02-20 21:05:03 +0900

    RecyclerView, LinearLayoutManager

--- app/src/main/java/com/example/myapplication/Practice/RecyclerViewActivity.kt
+++ app/src/main/java/com/example/myapplication/Practice/RecyclerViewActivity.kt
@@ -2,7 +2,17 @@ package com.example.myapplication.Practice
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Adapter
+import android.widget.LinearLayout
+import android.widget.TextView
+import androidx.appcompat.view.menu.MenuView
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.example.myapplication.R
+import kotlinx.android.synthetic.main.activity_recycler_view.*
 
 class RecyclerViewActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -12,7 +22,40 @@ class RecyclerViewActivity : AppCompatActivity() {
         val carList = ArrayList<CarForList>()
         for (i in 0 until 10) {
             carList.add(CarForList("" + i + "번째 자동차", "" + i + "순위 엔진"))
-        
+
         }
+        val adapter = RecyclerViewAdapter(carList, LayoutInflater.from(this@RecyclerViewActivity))
+        recycler_view.adapter = adapter
+        // 레이아웃 매니저
+        recycler_view.layoutManager = LinearLayoutManager(this@RecyclerViewActivity)
+    }
+}
+class RecyclerViewAdapter(
+    val itemList : ArrayList<CarForList>,
+    val inflater : LayoutInflater
+): RecyclerView.Adapter<RecyclerViewAdapter.ViewHolder>(){
+
+    class ViewHolder(itemView : View):RecyclerView.ViewHolder(itemView) {
+        val carName : TextView
+        val carEngine : TextView
+        // init 블럭, 초기화 블럭 이라고 한다. 클래스를 실행할 때 가장 먼저 실행된다고 한다.
+        init {
+            carName = itemView.findViewById(R.id.car_name)
+            carEngine = itemView.findViewById(R.id.car_engine)
+        }
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val view = inflater.inflate(R.layout.item_view,parent,false)
+        return ViewHolder(view)
+    }
+
+    override fun getItemCount(): Int {
+        return itemList.size
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        holder.carName.setText(itemList.get(position).name)
+        holder.carEngine.setText(itemList.get(position).engine)
     }
 }
